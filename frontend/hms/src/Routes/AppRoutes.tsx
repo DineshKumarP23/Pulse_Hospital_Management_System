8642efeae6b4e6389a6eac88617eee70d455c5cc
DineshKumarP23 <dineshkumar231202@gmail.com>
2026-09-09 16:06:50 +0530

    Front End Small Changes Done.

--- frontend/hms/src/Routes/AppRoutes.tsx
+++ frontend/hms/src/Routes/AppRoutes.tsx
@@ -8,20 +8,20 @@ import PatientDashboard from "../Layout/PatientDashboard";
 import PatientProfilePage from "../Pages/Patient/PatientProfilePage";
 import DoctorDashboard from "../Layout/DoctorDashboard";
 import DoctorProfilePage from "../Pages/Doctor/DoctorProfilePage";
-// import PatientAppointmentPage from "../Pages/Patient/PatientAppointmentPage";
-// import DoctorAppointmentPage from "../Pages/Doctor/DoctorAppointmentPage";
-// import DoctorAppointmentDetailsPage from "../Pages/Doctor/DoctorAppointmentDetailsPage";
-// import AdminMedicinePage from "../Pages/Admin/AdminMedicinePage";
+import PatientAppointmentPage from "../Pages/Patient/PatientAppointmentPage";
+import DoctorAppointmentPage from "../Pages/Doctor/DoctorAppointmentPage";
+import DoctorAppointmentDetailsPage from "../Pages/Doctor/DoctorAppointmentDetailsPage";
+import AdminMedicinePage from "../Pages/Admin/AdminMedicinePage";
 import NotFoundPage from "../Pages/NotFoundPage";
-// import AdminInventoryPage from "../Pages/Admin/AdminInventoryPage";
-// import AdminSalesPage from "../Pages/Admin/AdminSalesPage";
-// import AdminPatientPage from "../Pages/Admin/AdminPatientPage";
-// import AdminDoctorPage from "../Pages/Admin/AdminDoctorPage";
-// import AdminDashboardPage from "../Pages/Admin/AdminDashboardPage";
-// import DoctorDashboardPage from "../Pages/Doctor/DoctorDashboardPage";
-// import PatientDashboardPage from "../Pages/Patient/PatientDashboardPage";
-// import DoctorPatientPage from "../Pages/Doctor/DoctorPatientPage";
-// import DoctorPharmacyPage from "../Pages/Doctor/DoctorPharmacyPage";
+import AdminInventoryPage from "../Pages/Admin/AdminInventoryPage";
+import AdminSalesPage from "../Pages/Admin/AdminSalesPage";
+import AdminPatientPage from "../Pages/Admin/AdminPatientPage";
+import AdminDoctorPage from "../Pages/Admin/AdminDoctorPage";
+import AdminDashboardPage from "../Pages/Admin/AdminDashboardPage";
+import DoctorDashboardPage from "../Pages/Doctor/DoctorDashboardPage";
+import PatientDashboardPage from "../Pages/Patient/PatientDashboardPage";
+import DoctorPatientPage from "../Pages/Doctor/DoctorPatientPage";
+import DoctorPharmacyPage from "../Pages/Doctor/DoctorPharmacyPage";
 const AppRoutes = () => {
     return (
         <BrowserRouter>
@@ -29,25 +29,25 @@ const AppRoutes = () => {
                 <Route path='/login' element={<PublicRoute><LoginPage /></PublicRoute>} />
                 <Route path='/register' element={<PublicRoute><RegisterPage /></PublicRoute>} />
                 <Route path='/admin' element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>}>
-                    {/* <Route path="dashboard" element={<AdminDashboardPage />} />
+                    <Route path="dashboard" element={<AdminDashboardPage />} />
                     <Route path="medicine" element={< AdminMedicinePage />} />
                     <Route path="inventory" element={<AdminInventoryPage />} />
                     <Route path="sales" element={<AdminSalesPage />} />
                     <Route path="patients" element={<AdminPatientPage />} />
-                    <Route path="doctors" element={<AdminDoctorPage />} /> */}
+                    <Route path="doctors" element={<AdminDoctorPage />} />
                 </Route>
                 <Route path='/doctor' element={<ProtectedRoute><DoctorDashboard /></ProtectedRoute>}>
-                    {/* <Route path="dashboard" element={<DoctorDashboardPage />} /> */}
+                    <Route path="dashboard" element={<DoctorDashboardPage />} />
                     <Route path="profile" element={<DoctorProfilePage />} />
-                    {/* <Route path="appointments" element={<DoctorAppointmentPage />} />
+                    <Route path="appointments" element={<DoctorAppointmentPage />} />
                     <Route path="appointments/:id" element={<DoctorAppointmentDetailsPage />} />
                     <Route path="pharmacy" element={<DoctorPharmacyPage />} />
-                    <Route path="patients" element={<DoctorPatientPage />} /> */}
+                    <Route path="patients" element={<DoctorPatientPage />} />
                 </Route>
                 <Route path='/patient' element={<ProtectedRoute><PatientDashboard /></ProtectedRoute>}>
-                    {/* <Route path="dashboard" element={<PatientDashboardPage />} /> */}
+                    <Route path="dashboard" element={<PatientDashboardPage />} />
                     <Route path="profile" element={<PatientProfilePage />} />
-                    {/* <Route path="appointments" element={<PatientAppointmentPage />} /> */}
+                    <Route path="appointments" element={<PatientAppointmentPage />} />
                 </Route>
                 <Route path="/" element={<Navigate to="/login" replace />} />
                 <Route path='*' element={<NotFoundPage />} />
